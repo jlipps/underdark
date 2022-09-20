b9658e32c953a869feb7c794a4d4f42475d438d7
Jonathan Lipps <jlipps@gmail.com>
2022-09-20 10:51:26 -0700

    link to github repo

--- app/pages/about.mjs
+++ app/pages/about.mjs
@@ -20,6 +20,9 @@ export default function html({html, state}) {
         <h1>About the Author</h1>
         <figure class="image is-128x128"><img src="${arc.static(authorImage)}" class="is-rounded" /></figure>
         ${authorHtml}
+
+        <h1>Beyond the Technical Veil</h1>
+        Are you curious about the code that powers this site? I built it by hand (with lots of love of course), using standard web technologies plus a bit of help from some HTML and CSS frameworks. It's all open source for you to look at if you want: <a href="https://github.com/jlipps/underdark">github:jlipps/underdark</a>.
       </ud-content>
 
     </ud-layout>
