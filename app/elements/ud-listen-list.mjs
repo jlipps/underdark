f387d09e9b967f0bdd4ebc03384e1f928858393e
Jonathan Lipps <jlipps@gmail.com>
2022-09-21 10:36:41 -0700

    add more significant podcast callout on main page

--- app/elements/ud-listen-list.mjs
+++ app/elements/ud-listen-list.mjs
@@ -1,9 +1,18 @@
+import arc from '@architect/functions'
 import {POD_LINK_APPLE, POD_LINK_GOOGLE, POD_LINK_AMAZON, POD_LINK_SPOTIFY, RSS_PATH} from '../shared/constants.mjs'
 
 export default function UDListenList({html, state}) {
   const {attrs} = state
   const {size} = attrs
+  const showNotes = attrs['show-notes']
   const sizeClass = size ? `ud-size-${size}` : ''
+
+  const showNotesHtml = showNotes ? html`
+    <a class="button" href="/podcast">
+      <ud-icon icon="microphone"></ud-icon>
+      <img class="logo-button" src="${arc.static('img/underdark-logo-small.png')}" />
+    </a>
+  ` : ''
 
   return html`
     <style>
@@ -16,12 +25,19 @@ export default function UDListenList({html, state}) {
         font-style: italic;
         margin-right: 0.75rem;
       }
+
+      .button .logo-button {
+        height: 1rem;
+      }
     </style>
 
     <div class="buttons is-justify-content-center ${sizeClass}">
       <div class="leading-text">
         <slot></slot>
       </div>
+
+      ${showNotesHtml}
+
       <a class="button" href="${POD_LINK_SPOTIFY}" target="_blank">
         <ud-icon icon="brands spotify"></ud-icon>
         Spotify
