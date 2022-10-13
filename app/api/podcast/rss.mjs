279d7f6a5c8c9d000f059ac3c4900967cd268a43
Jonathan Lipps <jlipps@gmail.com>
2022-10-13 12:52:15 -0700

    use xml helper for rss feed api

--- app/api/podcast/rss.mjs
+++ app/api/podcast/rss.mjs
@@ -2,11 +2,6 @@ import {findPods} from '../../shared/queries.mjs'
 import {getFeedXml} from '../../shared/rss.mjs'
 
 export async function get (/*req*/) {
-  const body = getFeedXml(await findPods())
-  return {
-    headers: {
-      'Content-type': 'application/xml'
-    },
-    body,
-  }
+  const xml = getFeedXml(await findPods())
+  return {xml}
 }
