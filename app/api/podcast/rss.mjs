87694198f58f6bf2d997e92863d4494119b02d74
Jonathan Lipps <jlipps@gmail.com>
2022-10-13 13:38:36 -0700

    update cache control header for rss feed

--- app/api/podcast/rss.mjs
+++ app/api/podcast/rss.mjs
@@ -3,5 +3,10 @@ import {getFeedXml} from '../../shared/rss.mjs'
 
 export async function get (/*req*/) {
   const xml = getFeedXml(await findPods())
-  return {xml}
+  return {
+    headers: {
+      'cache-control': 'max-age=300'
+    },
+    xml
+  }
 }
