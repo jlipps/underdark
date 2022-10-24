941279022f8e06de2bba0e47c98e6148b6c559ae
Jonathan Lipps <jlipps@gmail.com>
2022-10-24 09:43:21 -0700

    fix rss feed content type header

--- app/api/podcast/rss.mjs
+++ app/api/podcast/rss.mjs
@@ -5,7 +5,8 @@ export async function get (/*req*/) {
   const xml = getFeedXml(await findPods())
   return {
     headers: {
-      'cache-control': 'no-cache'
+      'cache-control': 'no-cache',
+      'content-type': 'application/xml; charset=utf-8',
     },
     xml
   }
@@ -15,7 +16,6 @@ export async function head(req) {
   const res = await get(req)
   res.headers['content-length'] = res.xml.length
   delete res.xml
-  res.headers['content-type'] = 'application/xml; charset=utf-8'
   res.body = ''
   return res
 }
